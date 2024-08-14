f7c6ab1d99986ca3a9970f75f992a5bb3caa290e
ricidkumbara <ricidkumbara@gmail.com>
2024-08-14 20:44:03 +0700

    update

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,5 @@
 use sqlx::MySqlPool;
 
 pub async fn database_connection() -> Result<MySqlPool, sqlx::Error> {
-    MySqlPool::connect("mysql://root:root@localhost:3306/rnd").await
+    MySqlPool::connect(std::env::var("DATABASE_URL").expect("Databse URL must be set").as_str()).await
 }
